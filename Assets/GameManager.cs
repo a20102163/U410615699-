a89cfc6e2fecbf847427b8d94067a9e4f37ea782
a20102163 <a201021632@gmail.com>
2018-11-23 11:00:01 +0800

    q1

---
+++ Assets/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public void OnStartGame(string ScneneName)
+    {
+        Application.LoadLevel(ScneneName);
+    }
+
+    public void exitgame()
+    {
+        Application.Quit();
+    }
+}
