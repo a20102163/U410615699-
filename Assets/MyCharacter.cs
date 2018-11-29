0726b4d559319ff17792d9b90cfc2820ad6a8055
a20102163 <a201021632@yahoo.com>
2018-11-29 17:52:11 +0800

    q8

---
+++ Assets/MyCharacter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCharacter : MonoBehaviour {
+    public bool movable = false;
+    CharacterController controller;
+    public GameObject FinishText;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 speed = new Vector3(0, 0, 0);
+        if (movable)
+        {
+            speed.x = -5;
+        }
+        controller.SimpleMove(speed);
+	}
+
+    public void CharacterWalk() {
+        movable = true;
+        GetComponent<Animation>().Play("walk");
+    }
+    public void CharacterStop()
+    {
+        movable = false;
+        GetComponent<Animation>().Play("idle");
+    }
+}
+
---
+++ Assets/touch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public GameObject person;
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
